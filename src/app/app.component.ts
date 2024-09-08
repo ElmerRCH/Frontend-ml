90a5404a51f2c211a2890465281c49a6aeaf3702
Elmer <renehiguera115@gmail.com>
2024-09-07 19:39:57 -0700

    card-fist-disaign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent,MarcasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
