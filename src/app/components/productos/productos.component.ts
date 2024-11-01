032907a4da40ddafa8b615e0dd84ed297dd52efd
Elmer <renehiguera15@gmail.com>
2024-10-31 20:28:50 -0700

    format currency

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -27,6 +27,14 @@ export class ProductosComponent {
         }
       );
     });
-
-}
+    }
+  formatCurrency(amount: number): string {
+      const formattedAmount = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+      }).format(amount);
+      return formattedAmount.replace(/\s/g, '');
+    }
 }
