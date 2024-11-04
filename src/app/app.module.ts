434e54bfa0cd4704b427b8ff5ccc1ddeacfe9f8c
Elmer <renehiguera15@gmail.com>
2024-11-03 19:58:02 -0700

    loading screen component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 import { MarcasComponent } from './components/marcas/marcas.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductosComponent } from './components/productos/productos.component';
+import { LoadingIndicatorComponent } from './components/shared/loading-indicator/loading-indicator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     MarcasComponent,
-    ProductosComponent
+    ProductosComponent,
+    LoadingIndicatorComponent
   ],
   imports: [
     BrowserModule,
